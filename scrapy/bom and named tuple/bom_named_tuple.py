c265763cfd3fc25b5a20bceb9843b77112310ff2
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-01-13 13:31:49 +1100

    code on Friday 13 January  13:31:49 AEDT 2017

--- scrapy/bom and named tuple/bom_named_tuple.py
+++ scrapy/bom and named tuple/bom_named_tuple.py
@@ -1,19 +1,25 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 from scrapy import signals
-from scrapy.conf import settings
+from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
 from scrapy.xlib.pydispatch import dispatcher
 from multiprocessing.queues import Queue
 import scrapy
 import multiprocessing
 import datetime
+import collections
 
 
-# In[2]:
+# In[31]:
+
+wealther_data=collections.namedtuple('wealther_data','day,max_t,min_t,summary')
+
+
+# In[32]:
 
 class CrawlerWorker(multiprocessing.Process):
 
@@ -21,7 +27,7 @@ class CrawlerWorker(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.result_queue = result_queue
 
-        self.crawler = CrawlerProcess(settings)
+        self.crawler = CrawlerProcess(get_project_settings())
         #if not hasattr(project, 'crawler'):
         #    self.crawler.install()
         #self.crawler.configure()
@@ -41,7 +47,7 @@ class CrawlerWorker(multiprocessing.Process):
         
 
 
-# In[22]:
+# In[36]:
 
 class CanberraWealtherSpider(scrapy.Spider):
     name = "CanberraWealtherSpider"
@@ -50,19 +56,26 @@ class CanberraWealtherSpider(scrapy.Spider):
 
     def parse(self, response):
         forecast_tags=response.xpath('//div[@class="forecast"]')
+        i=0
+        today=datetime.datetime.today()
+        for forecast_eachday in forecast_tags:
 #注意这里子tag的写法，它是绝对路径。如果写成相对路径，forecast_eachday.xpath('//em[@class="max"]/text()').extract()
 #会出错。比如运行forecast_tags[0].xpath('//em[@class="max"]/text()').extract(),结果将是forecast_tag[0]-forecast_tag[6]七个
-#关于max的结果。所以子路径这里一定要用绝对路径。
-        for forecast_eachday in forecast_tags:
+#关于max的结果。所以子路径这里一定要用绝对路径
             Max_Temperature=forecast_eachday.xpath('dl/dd/em[@class="max"]/text()').extract()
             Min_Temperature=forecast_eachday.xpath('dl/dd/em[@class="min"]/text()').extract()
             if Min_Temperature == []:
                 Min_Temperature=['Not Exist']
             Summary=forecast_eachday.xpath('dl/dd[@class="summary"]/text()').extract()
-            yield{'max':Max_Temperature[0].encode('utf-8'),'min':Min_Temperature[0].encode('utf-8'),'summary':Summary[0].encode('utf-8')}
+            date=today+datetime.timedelta(i)
+            i = i+1
+            wealther=wealther_data(str(date.date()),Max_Temperature[0].encode('utf-8'),Min_Temperature[0].encode('utf-8'),Summary[0].encode('utf-8'))
+            yield {str(date.date()):wealther}
+            #yield wealther_data(str(date.date()),Max_Temperature[0].encode('utf-8'),Min_Temperature[0].encode('utf-8'),Summary[0].encode('utf-8'))
+            #yield{'max':Max_Temperature[0].encode('utf-8'),'min':Min_Temperature[0].encode('utf-8'),'summary':Summary[0].encode('utf-8')}
 
 
-# In[23]:
+# In[37]:
 
 def main():
     result_queue = Queue()
@@ -73,7 +86,7 @@ def main():
         print item
 
 
-# In[24]:
+# In[38]:
 
 if __name__ == '__main__':
     main()
